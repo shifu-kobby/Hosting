50e96abe4b9a89e906ab4d92a51de7db36a014ae
shifu-kobby <samuelarmahappiah@gmail.com>
2022-09-17 21:44:38 +0000

    added packages

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,11 +11,38 @@ export class HomeComponent implements OnInit {
   packages: Package[] = [
     {
       "name": "Beginner",
+      "price": "$15",
+      "benefits": [
+        {"included": true, "benefit": "Authentication"},
+        {"included": true, "benefit": "Analytics"},
+        {"included": true, "benefit": "Performance Monitoring"},
+        {"included": false, "benefit": "Cloud Messaging"},
+        {"included": false, "benefit": "Remote Config"},
+        {"included": false, "benefit": "A / B Testing"},
+      ]
+    },
+    {
+      "name": "Intermmediate",
       "price": "$20",
       "benefits": [
-        { "included": true, "benefit": "One Free Website Host" },
-        { "included": true, "benefit": "Limited Tools" },
-        { "included": false, "benefit": "Personal Coach" }
+        {"included": true, "benefit": "Authentication"},
+        {"included": true, "benefit": "Analytics"},
+        {"included": true, "benefit": "Performance Monitoring"},
+        {"included": true, "benefit": "Cloud Messaging"},
+        {"included": false, "benefit": "Remote Config"},
+        {"included": false, "benefit": "A / B Testing"},
+      ]
+    },
+    {
+      "name": "Professional",
+      "price": "$35",
+      "benefits": [
+        {"included": true, "benefit": "Authentication"},
+        {"included": true, "benefit": "Analytics"},
+        {"included": true, "benefit": "Performance Monitoring"},
+        {"included": true, "benefit": "Cloud Messaging"},
+        {"included": true, "benefit": "Remote Config"},
+        {"included": true, "benefit": "A / B Testing"},
       ]
     }
   ];
